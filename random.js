5089b6f2f8f5adf11f46859d129424a9a97442ff
JavierZarza <jzarzame@hotmail.com>
2015-06-03 09:57:28 +0200

    random version-2

--- random.js
+++ random.js
@@ -1,3 +1,14 @@
 /**
  * Created by javier on 3/06/15.
  */
+
+
+var numero  = Math.random();
+
+var str = ' MAYOR QUE 0.5';
+
+if (numero <=0.5){
+    str = ' MENOR QUE 0.5';
+}
+
+console.log('\n' + numero + str + '\n');
